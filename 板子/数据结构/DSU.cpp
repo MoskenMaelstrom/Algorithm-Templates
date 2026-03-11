1aa01978a82d9bd1b064cfe27f9698088f00839e
MoskenMaelstrom <485106943@qq.com>
2026-03-11 16:37:13 +0800

    Auto Update

--- 板子/数据结构/DSU.cpp
+++ 板子/数据结构/DSU.cpp
@@ -1,39 +1,34 @@
 
 struct DSU {
-	vector<int> f,siz;
-	
-	DSU() {}
-	DSU(int n) {init(n);}
-	
-	void init(int n) {
-		f.resize(n);
-		iota(f.begin(),f.end(),0);
-		siz.assign(n,1);
-	}
-	
-	int find(int x) {
-		while (x != f[x]) {
-			x = f[x] = f[f[x]];
-		}
-		return x;
-	}
-	
-	bool merge(int x,int y) {
-		x=find(x);
-		y=find(y);
-		if (x == y) return 0;
-		siz[x] += siz[y];
-		f[y]=x;
-		return 1;
-	}
-	
-	int esize(int x) {
-		return siz[find(x)];
-	}
-	
-	int count() {
-		unordered_set<int> root;
-		for(int i=0;i<f.size();i++) root.insert(find(i));
-		return root.size();
-	}
+    vector<int> f,siz;
+    int cnt;
+
+    DSU():cnt(0) {}
+    DSU(int n) {init(n);}
+
+    void init(int n) {
+        f.resize(n);
+        iota(f.begin(),f.end(),0);
+        siz.assign(n,1);
+        cnt=n;
+    }
+
+    int find(int x) {
+        while (x != f[x]) x=f[x]=f[f[x]];
+        return x;
+    }
+
+    bool merge(int x,int y) {
+        x=find(x);
+        y=find(y);
+        if (x == y) return 0;
+        siz[x]+=siz[y];
+        f[y]=x;
+        cnt--;
+        return 1;
+    }
+
+    int esize(int x) {
+        return siz[find(x)];
+    }
 };
