3ebc506ffb250e9a4f70ef4cffec4d9480c89fc6
MoskenMaelstrom <485106943@qq.com>
2026-03-17 14:14:43 +0800

    Auto Update

--- 板子/动态规划/01背包.cpp
+++ 板子/动态规划/01背包.cpp
@@ -10,7 +10,7 @@
 	// 二维费用背包,有两个约束条件,要求最后只能选k个,总容量>=sum
 	int T=0;
 	for(int i=1;i<=n;i++) T+=b[i];
-	vector dp(n+1,vector<int>(T+1,-1));// 因为容量有要求,所以需要-1代表不可达
+	vector dp(n+1,vector<int>(T+1,-1)); // 因为容量有要求,所以需要-1代表不可达
 	dp[0][0]=0;
 	for(int i=1;i<=n;i++) {
 		for(int j=i;j>=1;j--) {
@@ -21,3 +21,18 @@
 		}
 	}
 	cout<<*max_element(dp[k].begin()+sum,dp[k].end());
+
+
+	// map代替数组,示例是gcd作为背包容量
+	map<int,int> dp;
+	dp[0]=0;
+	for(int i=1;i<=n;i++) {
+		map<int,int> ndp; // map不能边遍历变改
+		for(auto [j,cost] : dp) {
+			int g=gcd(j,b[i]);
+			if (ndp.count(g) == 0 or a[i]+cost < ndp[g]) ndp[g]=a[i]+cost;
+		}
+		for(auto [j,cost] : ndp) {
+			if (dp.count(j) == 0 or cost < dp[j]) dp[j]=ndp[j];
+		}
+	}
