2ddb3dc50c2daea00e2f9f71db0d2f409f2a8c4a
MoskenMaelstrom <485106943@qq.com>
2026-07-15 19:36:21 +0800

    Auto Update

--- 板子/数学/高斯消元.cpp
+++ 板子/数学/高斯消元.cpp
@@ -1,82 +1,98 @@
 
-// 矩阵A的大小为 m*(n+1)
-// m方程个数,n未知数个数
-
+// A为增广矩阵
 // 加法:
 const double eps=1e-9;
-int gauss(vector<vector<double>>& A,int m,int n) {
+int gauss(vector<vector<double>>& A,int n,int m) {
     int row=0;
-    for (int col=0;col<n;col++) {
+    for(int col=0;col<m;col++) {
         int r=row;
-        for (int i=row+1;i<m;i++) {
+        for(int i=row+1;i<n;i++) {
             if (abs(A[i][col]) > abs(A[r][col])) r=i;
         }
         if (abs(A[r][col]) < eps) continue;
         if (r != row) swap(A[r],A[row]);
 
-        for (int k=row+1;k<m;k++) {
+        for(int k=row+1;k<n;k++) {
             if (abs(A[k][col]) > eps) {
                 double factor=A[k][col]/A[row][col];
-                for (int j=col;j<=n;j++) {
+                for(int j=col;j<=m;j++) {
                     A[k][j]-=factor*A[row][j];
                 }
             }
         }
         row++;// 秩++
-        if (row == m) break;// 后面几列都是自由变元
+        if (row == n) break;// 后面几列都是自由变元
     }
 
     // 无解
-    for (int i=row;i<m;i++) {
-        if (abs(A[i][n]) > eps) return -1;
+    for(int i=row;i<n;i++) {
+        if (abs(A[i][m]) > eps) return -1;
     }
 
     // 多解
-    if (row < n) return n-row;// 返回自由变元个数
+    if (row < m) return m-row;// 返回自由变元个数
 
     // 唯一解
-    for (int i=n-1;i>=0;i--) {
-        for (int j=i+1;j<n;j++) {
-            A[i][n]-=A[i][j]*A[j][n];
+    for(int i=m-1;i>=0;i--) {
+        for(int j=i+1;j<m;j++) {
+            A[i][m]-=A[i][j]*A[j][m];
         }
-        A[i][n]/=A[i][i];
+        A[i][m]/=A[i][i];
     }
     return 0;
 }
 
 // 异或:
-int gauss(vector<vector<int>>& A,int m,int n) {
+int gauss(vector<vector<int>>& A,int n,int m) {
     int row=0;
-    for (int col=0;col<n;col++) {
+    for(int col=0;col<m;col++) {
         int r=row;
-        while (r < m and A[r][col] == 0) r++;
-        if (r == m) continue;// 这列是自由变元
+        while (r < n and A[r][col] == 0) r++;
+        if (r == n) continue;// 这列是自由变元
         if (r != row) swap(A[r],A[row]);
 
-        for (int k=row+1;k<m;k++) {
+        for(int k=row+1;k<n;k++) {
             if (A[k][col] == 1) {
-                for (int j=col;j<=n;j++) {
+                for(int j=col;j<=m;j++) {
                     A[k][j]^=A[row][j];
                 }
             }
         }
         row++;// 秩++
-        if (row == m) break;// 后面几列都是自由变元
+        if (row == n) break;// 后面几列都是自由变元
     }
 	
     // 无解
-    for (int i=row;i<m;i++) {
-        if (A[i][n] != 0) return -1;
+    for(int i=row;i<n;i++) {
+        if (A[i][m] != 0) return -1;
     }
 
     // 多解
-    if (row < n) return n-row;// 自由变元个数
+    if (row < m) return m-row;// 自由变元个数
 
-    // 回代求解，唯一解
-    for (int i=n-1;i>=0;i--) {
-        for (int j=i+1;j<n;j++) {
-            if (A[i][j]) A[i][n]^=A[j][n];
+    // 回代，唯一解
+    for(int i=m-1;i>=0;i--) {
+        for(int j=i+1;j<m;j++) {
+            if (A[i][j]) A[i][m]^=A[j][m];
         }
     }
     return 0;
 }
+
+// Gauss-Jordan消元
+	vector<bitset<1001>> A(n);
+    int row=0;
+    vector<int> mark(n,-1);
+    for(int col=0;col<m;col++) {
+        int r=row;
+        while (r < n and !A[r][col]) r++;
+        if (r == n) continue;
+        if (r != row) swap(A[r],A[row]);
+        mark[row]=col;
+
+        for(int k=0;k<n;k++) {
+            if (k != row and A[k][col]) A[k]^=A[row];
+        }
+        row++;
+        if (row == n) break;
+    }
